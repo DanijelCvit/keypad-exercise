cc9cc0536fa18f53c256b1f5e70b606ea5b3414e
Danijel C <hyf@danijel.nl>
2021-10-29 20:13:09 +0200

    Removes console.log lines

--- src/listeners/keypadListeners.js
+++ src/listeners/keypadListeners.js
@@ -1,6 +1,5 @@
 import { handleKeyPress } from "../handlers/keypadHandlers.js";
 
 export const keyPress = (event) => {
-  console.log(event.target.textContent);
   handleKeyPress(event.target);
 };
