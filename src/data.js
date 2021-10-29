9481d4ac4bdcfb9dccc37f7118a3b65fbbaf6d77
Danijel C <hyf@danijel.nl>
2021-10-29 20:32:51 +0200

    Changed path to audio files

--- src/data.js
+++ src/data.js
@@ -3,51 +3,51 @@ export const keypadData = {
   keys: [
     {
       char: "1",
-      dialTone: "../../public/audio/dtmf/dtmf-1.mp3",
+      dialTone: "../../public/audio/dtmf-1.mp3",
     },
     {
       char: "2",
-      dialTone: "../../public/audio/dtmf/dtmf-2.mp3",
+      dialTone: "../../public/audio/dtmf-2.mp3",
     },
     {
       char: "3",
-      dialTone: "../../public/audio/dtmf/dtmf-3.mp3",
+      dialTone: "../../public/audio/dtmf-3.mp3",
     },
     {
       char: "4",
-      dialTone: "../../public/audio/dtmf/dtmf-4.mp3",
+      dialTone: "../../public/audio/dtmf-4.mp3",
     },
     {
       char: "5",
-      dialTone: "../../public/audio/dtmf/dtmf-5.mp3",
+      dialTone: "../../public/audio/dtmf-5.mp3",
     },
     {
       char: "6",
-      dialTone: "../../public/audio/dtmf/dtmf-6.mp3",
+      dialTone: "../../public/audio/dtmf-6.mp3",
     },
     {
       char: "7",
-      dialTone: "../../public/audio/dtmf/dtmf-7.mp3",
+      dialTone: "../../public/audio/dtmf-7.mp3",
     },
     {
       char: "8",
-      dialTone: "../../public/audio/dtmf/dtmf-8.mp3",
+      dialTone: "../../public/audio/dtmf-8.mp3",
     },
     {
       char: "9",
-      dialTone: "../../public/audio/dtmf/dtmf-9.mp3",
+      dialTone: "../../public/audio/dtmf-9.mp3",
     },
     {
       char: "*",
-      dialTone: "../../public/audio/dtmf/dtmf-star.mp3",
+      dialTone: "../../public/audio/dtmf-star.mp3",
     },
     {
       char: "0",
-      dialTone: "../../public/audio/dtmf/dtmf-0.mp3",
+      dialTone: "../../public/audio/dtmf-0.mp3",
     },
     {
       char: "#",
-      dialTone: "../../public/audio/dtmf/dtmf-hash.mp3",
+      dialTone: "../../public/audio/dtmf-hash.mp3",
     },
   ],
 };
